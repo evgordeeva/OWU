debc3b22cbdc7a8edb0bf6c49981b423861c96ab
evgenyagordeeva <gordeevevgenya@gmail.com>
2020-10-28 17:35:22 +0200

    homework5-1

---
+++ Homework/Homework5/hm5.js
@@ -0,0 +1,65 @@
+let word = 'hello';
+let name = 'owu';
+let browser = 'com';
+let browser2 = 'ua';
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let num5 = 3.14;
+let num6 = 2.7;
+let num7 = 16;
+let boolean = true;
+let boolean2 = false;
+
+console.log(word);
+alert(word);
+document.write('<p>word</p>');
+
+console.log(name);
+alert(name);
+document.write('<p>name</p>');
+
+console.log(browser);
+alert(browser);
+document.write('<p>browser</p>');
+
+console.log(browser2);
+alert(browser2);
+document.write('<p>browser2</p>');
+
+console.log(num1);
+alert(num1);
+document.write('<p>num1</p>');
+
+console.log(num2);
+alert(num2);
+document.write('<p>num2</p>');
+
+console.log(num3);
+alert(num3);
+document.write('<p>num3</p>');
+
+console.log(num4);
+alert(num4);
+document.write('<p>num4</p>');
+
+console.log(num5);
+alert(num5);
+document.write('<p>num5</p>');
+
+console.log(num6);
+alert(num6);
+document.write('<p>num6</p>');
+
+console.log(num7);
+alert(num7);
+document.write('<p>num7</p>');
+
+console.log(boolean);
+alert(boolean);
+document.write('<p>boolean</p>');
+
+console.log(boolean2);
+alert(boolean2);
+document.write('<p>boolean2</p>');
